85ea6dfb21f75540bad649014f8f78fecd18e3f8
hacylu <hacylu@gmail.com>
2019-09-19 12:03:07 -0400

    Add files via upload

---
+++ Lfind_featureidx_by_excluded_name.m
@@ -0,0 +1,18 @@
+function [set_idx,datas,feature_lists]=Lfind_featureidx_by_excluded_name(feature_name_set,data,feature_list)
+
+set_idx=[];
+for j=1:length(feature_name_set)
+    feature_name=feature_name_set{j};
+    for i=1:length(feature_list)
+        if ~isempty(strfind(feature_list{i},feature_name))
+            set_idx=[set_idx i];
+        end
+    end
+end
+if size(data,2)>1
+    datas=data(:,setdiff(1:size(data,2),set_idx));
+else
+    datas=data(setdiff(1:length(feature_list),set_idx));
+end
+
+feature_lists=feature_list(setdiff(1:length(feature_list),set_idx));
